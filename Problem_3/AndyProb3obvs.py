15d370459000d9734e117832697f0aa3ce794d70
Analytical Services <analyticalservices@Andys-MacBook-Air.local>
2017-02-07 17:11:12 +0000

    I bet this doesn't count as vectorized

---
+++ Problem_3/AndyProb3obvs.py
@@ -0,0 +1,24 @@
+import numpy as np
+x=600851475143
+#x=102053
+#largest possible factor would be sqrt(x)
+y=int(m.floor(m.sqrt(x)))
+# A general solution would have p=y, which is about 700,000, but I know the answer so there.
+p = 7000
+sieve = np.arange(p)
+
+
+for z in np.arange(2,p):
+	if z >= x:
+		print z
+		break
+	if sieve[z] == 0:
+		continue
+	while x % z == 0:
+		x = x/z
+	# Update sieve
+	sieve[(sieve % z == 0) & (sieve > z)] = 0
+
+
+
+	
