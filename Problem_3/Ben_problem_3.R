c24776a5da93ea1b0b6a97dd2a729bc91186893e
bfortescue <bfortescue@gmail.com>
2017-02-07 14:50:36 +0000

    Update Ben_problem_3.R

--- Problem_3/Ben_problem_3.R
+++ Problem_3/Ben_problem_3.R
@@ -1,15 +1,18 @@
 testnum <- 600851475143
-a=c(1:floor(sqrt(testnum)))
 
-# Get all factors (may not be prime)
+# We only need factors up to the root, can get the other ones by dividing.
+a=c(1:floor(sqrt(testnum)))
 smallfactors=a[testnum%%a==0]
 bigfactors=testnum/smallfactors
 
 # Get all factors in ascending order, except 1 and the number
 allfactors=c(smallfactors[2:length(smallfactors)],rev(bigfactors[2:length(bigfactors)]))
+
 # Get the outer remainder array (every factor%%every factor)
 outer_fac=outer(allfactors,allfactors,"%%")
-# Biggest prime is the factor with non-zero remainder for every smaller factor.  Corresponds to the last row in outer_fac with no zero in the lower triangle, so we find that.
-filled_upper=outer_fac*lower.tri(outer_fac)+upper.tri(outer_fac,diag=TRUE) #Fill in the upper triangle
+
+# Biggest prime is the factor with non-zero remainder for every smaller factor.
+# Corresponds to the last row in outer_fac with no zero in the lower triangle, so we find that.
+filled_upper=outer_fac+upper.tri(outer_fac,diag=TRUE) #Fill in the upper triangle
 check_zeros=apply(filled_upper,1,prod) # Multiply through to find the zeroes.  A bit lazy.
 allfactors[which.max(check_zeros)] #Find the last non-zero
