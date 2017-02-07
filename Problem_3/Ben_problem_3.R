ea6ad6a03fdb408533ec70c6b96eb97e07c47c9d
bfortescue <bfortescue@gmail.com>
2017-02-07 14:58:02 +0000

    Update Ben_problem_3.R

--- Problem_3/Ben_problem_3.R
+++ Problem_3/Ben_problem_3.R
@@ -14,5 +14,5 @@ outer_fac=outer(allfactors,allfactors,"%%")
 # Biggest prime is the factor with non-zero remainder for every smaller factor.
 # Corresponds to the last row in outer_fac with no zero in the lower triangle, so we find that.
 filled_upper=outer_fac+upper.tri(outer_fac,diag=TRUE) #Fill in the upper triangle and diagonal
-check_zeros=apply(filled_upper,1,prod) # Multiply through to find the zeroes.  A bit lazy.
-allfactors[which.max(check_zeros)] #Find the last non-zero
+check_zeros=apply(filled_upper,1,prod) # Multiply through the rows to find those with a zero.  A bit lazy.
+allfactors[which.max(check_zeros)] #Find the last non-zero and corresponding factor
