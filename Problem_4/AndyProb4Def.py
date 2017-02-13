ad052e4a3804b07582ddfc328ac108a00143647b
Analytical Services <analyticalservices@Andys-MacBook-Air.local>
2017-02-13 12:38:40 +0000

    This isn't quick, or vectorised, or commented, or big, or clever

--- Problem_4/AndyProb4Def.py
+++ Problem_4/AndyProb4Def.py
@@ -1,1 +1,13 @@
 #test post please ignore
+import numpy as np
+def isPal(x):
+	return str(x)==str(x)[::-1];
+
+thou = np.arange(100,1000)
+pals = []
+for i in thou:
+	for j in np.arange(i+1,1000):
+		if(isPal(i*j)):
+			pals.append(i*j)
+
+print(max(pals))
